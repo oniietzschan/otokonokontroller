821ede8444d5aaf6e04ad53edc78ae572372531c
shru <shrudc@gmail.com>
2017-11-28 15:46:29 -0500

    Test CI.

--- spec.lua
+++ spec.lua
@@ -6,4 +6,15 @@ describe('Otokonokontroller:', function()
   before_each(function()
     Otokonokontroller = require 'otokonokontroller'
   end)
+
+  describe('When creating a new controller', function()
+    local controller
+
+    before_each(function ()
+    end)
+
+    it('Should initialize successfully', function ()
+      controller = Otokonokontroller.newController()
+    end)
+  end)
 end)
